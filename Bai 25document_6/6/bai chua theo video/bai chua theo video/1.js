f5c7ba68f0525e65dd5b230db672fecd7caf203c
levantai <levantai@gmail.com>
2018-06-04 15:08:22 +0700

    Second commit

---
+++ Bai 25document_6/6/bai chua theo video/bai chua theo video/1.js
@@ -0,0 +1,17 @@
+﻿ $(function(){
+	$('.navbar-right li:nth-child(2) a')
+	.click(function(){
+		$('body').animate({scrollTop:750});
+		return false; 
+	})
+
+	$('.navbar-right li:nth-child(3) a')
+	.click(function(){
+		$('body').animate({scrollTop:1100});
+		return false; 
+	})
+
+
+})  
+	 
+     
